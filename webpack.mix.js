00cf5b1e8c408e7bc640a1be007b327528e2799a
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2022-01-02 15:30:58 +0300

    Add auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.ts('resources/js/app.tsx', 'public/js')
-   .react()
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+   .react();
+
+mix.postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+]);

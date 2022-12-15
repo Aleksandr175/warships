7f3fc8432c9834f2c21e16d884b92444386b9777
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2022-12-14 22:27:51 -0300

    Refactor, create logs of battle

--- app/Services/BattleService.php
+++ app/Services/BattleService.php
@@ -22,32 +22,22 @@ use Illuminate\Support\Facades\Auth;
 
 class BattleService
 {
-    private $cityId              = null;
-    private $coordX              = null;
-    private $coordY              = null;
-    private $fleetDetails        = [];
-    private $updatedFleetDetails = [];
-    private $repeating           = false;
-    private $taskType            = null;
-    private $targetCity          = null;
-    private $taskTypeId          = null;
-
     // handle battle process
     public function handle(Fleet $fleet)
     {
         $targetCity = City::find($fleet->target_city_id);
 
-        $fleetDetails = FleetDetail::getFleetDetails([$fleet->id]);
+        $attackingFleetDetails = FleetDetail::getFleetDetails([$fleet->id])->toArray();
 
         $warshipsDictionary = WarshipDictionary::get();
 
         if ($targetCity->city_dictionary_id === CityDictionary::PIRATE_BAY) {
-            dump('pirate bay');
+            $defendingFleetDetails = [];
 
             // get warships for pirate bay
-            $enemyWarships = $targetCity->warships;
-            foreach ($enemyWarships as $warship) {
-                $enemyFleetDetails[] = [
+            $defendingWarships = $targetCity->warships;
+            foreach ($defendingWarships as $warship) {
+                $defendingFleetDetails[] = [
                     'warship_id' => $warship['warship_id'],
                     'qty'        => $warship['qty']
                 ];
@@ -55,58 +45,60 @@ class BattleService
 
             // set needed data, like health
             foreach ($warshipsDictionary as $warshipDictionary) {
-                for ($i = 0, $iMax = count($fleetDetails); $i < $iMax; $i++) {
-                    if ($fleetDetails[$i]['warship_id'] === $warshipDictionary['id']) {
-                        $fleetDetails[$i]['health'] = $warshipDictionary['health'];
+                for ($i = 0, $iMax = count($attackingFleetDetails); $i < $iMax; $i++) {
+                    if ($attackingFleetDetails[$i]['warship_id'] === $warshipDictionary['id']) {
+                        $attackingFleetDetails[$i]['health'] = $warshipDictionary['health'];
                         break;
                     }
                 }
 
-                for ($i = 0, $iMax = count($enemyFleetDetails); $i < $iMax; $i++) {
-                    if ($enemyFleetDetails[$i]['warship_id'] === $warshipDictionary['id']) {
-                        $enemyFleetDetails[$i]['health'] = $warshipDictionary['health'];
+                for ($i = 0, $iMax = count($defendingFleetDetails); $i < $iMax; $i++) {
+                    if ($defendingFleetDetails[$i]['warship_id'] === $warshipDictionary['id']) {
+                        $defendingFleetDetails[$i]['health'] = $warshipDictionary['health'];
                         break;
                     }
                 }
             }
 
+            $logAttacking  = [];
+            $logDefending  = [];
+            $round = 0;
+
             // calculate rounds while we have warships on each side
             do {
-                $fleetForce            = 0;
-                $enemyForce            = 0;
-                $fleetTypes            = 0;
-                $pirateBayWarshipTypes = 0;
+                $attackingForce = 0;
+                $defendingForce = 0;
 
                 foreach ($warshipsDictionary as $warshipDictionary) {
-                    foreach ($fleetDetails as $fleetDetail) {
-                        if ($fleetDetail['warship_id'] === $warshipDictionary['id']) {
-                            $fleetForce += $fleetDetail['qty'] * $warshipDictionary['attack'];
-                            ++$fleetTypes;
+                    foreach ($attackingFleetDetails as $detail) {
+                        if ($detail['warship_id'] === $warshipDictionary['id']) {
+                            $attackingForce += ceil($detail['qty']) * $warshipDictionary['attack'];
                             break;
                         }
                     }
 
-                    foreach ($enemyFleetDetails as $fleetDetail) {
-                        if ($fleetDetail['warship_id'] === $warshipDictionary['id']) {
-                            $enemyForce += $fleetDetail['qty'] * $warshipDictionary['attack'];
-                            ++$pirateBayWarshipTypes;
+                    foreach ($defendingFleetDetails as $detail) {
+                        if ($detail['warship_id'] === $warshipDictionary['id']) {
+                            $defendingForce += ceil($detail['qty']) * $warshipDictionary['attack'];
                             break;
                         }
                     }
                 }
 
-                $damageToEachType = $fleetForce / $pirateBayWarshipTypes;
+                $attackingDamageToEachType = $attackingForce / count($defendingWarships);
+                $defendingDamageToEachType = $defendingForce / count($attackingFleetDetails);
 
-                [$enemyFleetDetails, $pirateBayWarshipTypes] = $this->shoot($damageToEachType, $enemyFleetDetails, $pirateBayWarshipTypes);
+                [$defendingFleetDetails, $logAttacking[$round]] = $this->shoot($attackingDamageToEachType, $defendingFleetDetails);
+                [$attackingFleetDetails, $logDefending[$round]] = $this->shoot($defendingDamageToEachType, $attackingFleetDetails);
 
-                $enemyDamageToEachType = $enemyForce / $fleetTypes;
-
-                [$fleetDetails, $fleetTypes] = $this->shoot($enemyDamageToEachType, $fleetDetails, $fleetTypes);
-            } while ($pirateBayWarshipTypes > 0 && $fleetTypes > 0);
+                $round++;
+            } while (count($defendingFleetDetails) > 0 && count($attackingFleetDetails) > 0);
 
 
-            dd($fleetDetails, $enemyFleetDetails);
+            dump('LOGS', $logAttacking, $logDefending);
+            dd($attackingFleetDetails, $defendingFleetDetails);
             // TODO calculate result of whole battle
+            // TODO put logs to db
             // ...
         }
 
@@ -122,27 +114,56 @@ class BattleService
 
     }
 
-    public function shoot($damage, $warships, $warshipTypes)
+    /**
+     * @param $damage - int
+     * @param $warships
+     *
+     * @return array
+     */
+    public function shoot($damage, $warships): array
     {
+        $restDamage = 0;
+        $log        = [];
+
+        dump('SHOOT');
+
         for ($i = 0, $iMax = count($warships); $i < $iMax; $i++) {
             $wholeHealth = $warships[$i]['qty'] * $warships[$i]['health'];
+            $restDamage  += $damage;
+            $logDamage   = $restDamage;
 
-            $wholeHealth -= $damage;
+            $startQty = ceil($warships[$i]['qty']);
 
-            if ($wholeHealth < 0) {
+            // if we did 100 damage, but health was 80 -> it means that 20 damage will be done to another warship type
+            // we save 20 to $restDamage
+            if ($wholeHealth < $restDamage) {
+                $restDamage  -= $wholeHealth;
                 $wholeHealth = 0;
+            } else {
+                $wholeHealth -= $restDamage;
+                $restDamage  = 0;
             }
 
-            $warships[$i]['qty'] = ceil($wholeHealth / $warships[$i]['health']);
+            $warships[$i]['qty'] = $wholeHealth / $warships[$i]['health'];
+            //dump('$warships[$i]', $warships[$i], $wholeHealth, $restDamage);
 
-            if ($warships[$i]['qty'] < 1) {
-                --$warshipTypes;
+            $log[] = [
+                'destroyed'  => $startQty - ceil($warships[$i]['qty']),
+                'warship_id' => $warships[$i]['warship_id'],
+                'damage'     => $logDamage,
+            ];
+
+            if ($warships[$i]['qty'] === 0) {
                 array_splice($warships, $i, 1);
                 $i--;
                 $iMax--;
+
+                if (count($warships) === 0) {
+                    break;
+                }
             }
         }
 
-        return [$warships, $warshipTypes];
+        return [$warships, $log];
     }
 }

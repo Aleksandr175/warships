db8fb98730e423e7a64bca603ab61709007f8e62
Aleksandr Pogrebitskiy <a.pogr@yandex.ru>
2024-03-12 00:02:25 -0300

    Add more recipes for improvements

--- database/seeders/WarshipImprovementRecipeSeeder.php
+++ database/seeders/WarshipImprovementRecipeSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\WarshipImprovementRecipe;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class WarshipImprovementRecipeSeeder extends Seeder
@@ -65,6 +64,90 @@ class WarshipImprovementRecipeSeeder extends Seeder
                 'improvement_type'    => 'health',
                 'level'               => $i,
                 'resource_id'         => config('constants.RESOURCE_IDS.CARD_CARAVEL_HEALTH'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.GALERA'),
+                'improvement_type'    => 'attack',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_GALERA_ATTACK'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.GALERA'),
+                'improvement_type'    => 'capacity',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_GALERA_CAPACITY'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.GALERA'),
+                'improvement_type'    => 'health',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_GALERA_HEALTH'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.FRIGATE'),
+                'improvement_type'    => 'attack',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_FRIGATE_ATTACK'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.FRIGATE'),
+                'improvement_type'    => 'capacity',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_FRIGATE_CAPACITY'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.FRIGATE'),
+                'improvement_type'    => 'health',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_FRIGATE_HEALTH'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.BATTLESHIP'),
+                'improvement_type'    => 'attack',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_BATTLESHIP_ATTACK'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.BATTLESHIP'),
+                'improvement_type'    => 'capacity',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_BATTLESHIP_CAPACITY'),
+                'qty'                 => $i,
+                'percent_improvement' => $i,
+            ]);
+
+            WarshipImprovementRecipe::create([
+                'warship_id'          => config('constants.WARSHIPS.BATTLESHIP'),
+                'improvement_type'    => 'health',
+                'level'               => $i,
+                'resource_id'         => config('constants.RESOURCE_IDS.CARD_BATTLESHIP_HEALTH'),
                 'qty'                 => $i,
                 'percent_improvement' => $i,
             ]);
